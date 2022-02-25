e4608d1a68ab936ea3685c7ade8fc524d1546aa8
danielgelfand <>
2022-02-25 11:42:36 -0500

    a5 navigation cleanup to new explore

--- public/tuiter/NavigationSideBar/index.js
+++ public/tuiter/NavigationSideBar/index.js
@@ -4,28 +4,28 @@ const NavigationSidebar = () => {
                 <a class="list-group-item" href="/">
                     <i class="fab fa-twitter"></i>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="home.html">
                     <i class="fas fa-home"></i> <div class="d-none d-xl-inline">Home</div>
                 </a>
-                <a class="list-group-item active" href="/">
+                <a class="list-group-item active" href="explore.html">
                     <i class="fas fa-hashtag"></i> <div class="d-none d-xl-inline">Explore</div>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="notifications.html">
                     <i class="fas fa-bell"></i> <div class="d-none d-xl-inline">Notifications</div>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="messages.html">
                      <i class="fas fa-envelope"></i> <div class="d-none d-xl-inline">Messages</div>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="bookmarks.html">
                     <i class="fas fa-bookmark"></i> <div class="d-none d-xl-inline">Bookmarks</div>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="lists.html">
                     <i class="fas fa-list"></i> <div class="d-none d-xl-inline">Lists</div>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="profile.html">
                     <i class="fas fa-user"></i> <div class="d-none d-xl-inline">Profile</div>
                 </a>
-                <a class="list-group-item" href="/">
+                <a class="list-group-item" href="more.html">
                     <i class="fas fa-circle"></i> <div class="d-none d-xl-inline">More</div>
                 </a>
             
